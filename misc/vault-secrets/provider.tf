189d2305ff9f9d8d1cf2e875a61299f9f95873c2
chinni venkatapavan <chinnivenkatapavan@gmil.com>
2025-01-25 10:02:41 +0530

    Added New concept in my list

--- misc/vault-secrets/provider.tf
+++ misc/vault-secrets/provider.tf
@@ -7,7 +7,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "project-setup-1"
     storage_account_name = "d82tfstate"
-    container_name       = "vault-tf"
+    container_name       = "vault-tool-tf"
     key                  = "dev.tfstate"
   }
 }
