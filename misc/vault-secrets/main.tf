7a56ba47602ffdbc4bb33d660e2dbb7bd79dfb45
chinni venkatapavan <chinnivenkatapavan@gmil.com>
2025-01-26 12:55:09 +0530

    Added New concept in my list

--- misc/vault-secrets/main.tf
+++ misc/vault-secrets/main.tf
@@ -1,15 +1,11 @@
-resource "vault_mount" "main" {
-  for_each    = var.secrets
-  path        = each.key
-  type        = "kv"
-  options     = { version = "1" }
-  description = each.key
+module "create-secrets" {
+  for_each = var.secrets
+  source = "./create-secrets"
+  kv_path = each.key
+  secrets = each.value
+
 }
 
-resource "vault_kv_secret" "secret" {
-  path = "infra/ssh"
-  data_json = jsonencode( var.secrets["infra"].ssh )
-}
 
 variable "secrets" {
   default = {
