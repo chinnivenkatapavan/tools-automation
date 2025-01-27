b1b06f14c530c2d6d3adb0ad4ddce62508678db7
chinni venkatapavan <chinnivenkatapavan@gmil.com>
2025-01-27 06:34:32 +0530

    Added New concept in my list

--- misc/vault-secrets/main.tf
+++ misc/vault-secrets/main.tf
@@ -27,6 +27,33 @@ variable "secrets" {
         MONGO = "true"
         MONGO_URL = "mongodb://mongodb-dev.azdevops2704.online:27017/catalogue"
       }
+      user = {
+        MONGO=true
+        REDIS_URL = "redis://redis-dev.azdevops2704.online:6379"
+        MONGO_URL = "mongodb://mongodb-dev.azdevops2704.online:27017/users"
+      }
+      cart = {
+        REDIS_HOST = "redis-dev.azdevops2704.online"
+        CATALOGUE_HOST = "catalogue-dev.azdevops2704.online"
+        CATALOGUE_PORT = "8080"
+      }
+      payment = {
+        CART_HOST = "cart-DEV.azdevops2704.online"
+        CART_PORT = "8080"
+        USER_HOST = "user-dev.azdevops2704.online"
+        USER_PORT = "8080"
+        AMQP_HOST = "rabbitmq-dev.azdevops2704.online"
+        AMQP_USER = "roboshop"
+        AMQP_PASS = "roboshop123"
+      }
+      mysql = {
+        MYSQL_ROOTUSER = "RoboShop@1"
+      }
+      rabbitmq = {
+        AMQP_USER = "roboshop"
+        AMQP_PASS = "roboshop123"
+
+      }
     }
   }
 }
